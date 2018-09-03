7a64eaedecca9317af9de291065f3023ce5ba24f
李楠楠 <nannan.li@tpadsz.cn>
2018-09-03 10:10:40 +0800

    灯控App蓝牙操作添加蓝牙组

--- src/main/java/com/tpadsz/after/controller/AppOperateController.java
+++ src/main/java/com/tpadsz/after/controller/AppOperateController.java
@@ -2,12 +2,12 @@ package com.tpadsz.after.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.tpadsz.after.api.RecordBillService;
 import com.tpadsz.after.entity.*;
 import com.tpadsz.after.entity.dd.ResultDict;
 import com.tpadsz.after.service.AppOperateService;
 import com.tpadsz.after.service.LightUserService;
 import com.tpadsz.after.service.PairingService;
-import com.tpadsz.after.api.RecordBillService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,9 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -43,7 +42,8 @@ public class AppOperateController extends BaseDecodedController {
     private RecordBillService billService;
 
     @RequestMapping(value = "/blue", method = RequestMethod.POST)
-    public void bluethoothConnect(@ModelAttribute("decodedParams") JSONObject params, ModelMap model) {
+    public void bluethoothConnect(@ModelAttribute("decodedParams") JSONObject
+                                              params, ModelMap model) {
 
         String uid = params.getString("uid");
         JSONArray array = params.getJSONArray("lightGroup");
@@ -56,8 +56,10 @@ public class AppOperateController extends BaseDecodedController {
             bluethoothConnect = new BluethoothConnect();
             bluethoothConnect.setId(id);
             bluethoothConnect.setUid(uid);
-            bluethoothConnect.setDevice_id(array.getJSONObject(i).getString("deviceId"));
-            bluethoothConnect.setConnectStatus(array.getJSONObject(i).getString("connectStatus"));
+            bluethoothConnect.setDevice_id(array.getJSONObject(i).getString
+                    ("deviceId"));
+            bluethoothConnect.setConnectStatus(array.getJSONObject(i)
+                    .getString("connectStatus"));
             appOperateService.connectToBluetoothLog(bluethoothConnect);
 
             //蓝牙配对和绑定关系判断
@@ -65,11 +67,14 @@ public class AppOperateController extends BaseDecodedController {
             lightPairing = pairingService
                     .findPairingInfoByLightUid(uid);
             if (lightPairing == null) {
-                pairingService.savePairingInfo(uid, array.getJSONObject(i).getString("deviceId"));
+                pairingService.savePairingInfo(uid, array.getJSONObject(i)
+                        .getString("deviceId"));
             } else {
                 String deviceIds = lightPairing.getName();
-                net.sf.json.JSONArray jsonArray = net.sf.json.JSONArray.fromObject(deviceIds);
-                if (!jsonArray.contains(array.getJSONObject(i).getString("deviceId"))) {
+                net.sf.json.JSONArray jsonArray = net.sf.json.JSONArray
+                        .fromObject(deviceIds);
+                if (!jsonArray.contains(array.getJSONObject(i).getString
+                        ("deviceId"))) {
                     jsonArray.add(array.getJSONObject(i).getString("deviceId"));
                     pairingService.updatePairingInfo(uid, jsonArray
                             .toString());
@@ -78,13 +83,17 @@ public class AppOperateController extends BaseDecodedController {
             isLogin = pairingService.findLoginState(uid);
             if ("1".equals(isLogin)) {
                 LightBinding lightBinding = pairingService
-                        .findBindingInfoByDeviceId(array.getJSONObject(i).getString("deviceId"));
+                        .findBindingInfoByDeviceId(array.getJSONObject(i)
+                                .getString("deviceId"));
                 if (lightBinding == null) {
-                    LightBinding lightBinding2 = new LightBinding(array.getJSONObject(i).getString("deviceId"),
-                            array.getJSONObject(i).getString("mac"), uid, null, new Date(), null, null);
+                    LightBinding lightBinding2 = new LightBinding(array
+                            .getJSONObject(i).getString("deviceId"),
+                            array.getJSONObject(i).getString("mac"), uid,
+                            null, new Date(), null, null);
                     pairingService.saveBindingInfo(lightBinding2);
                 } else if (!uid.equals(lightBinding.getLightUid())) {
-                    pairingService.updateBindingInfo(uid, array.getJSONObject(i).getString("deviceId"));
+                    pairingService.updateBindingInfo(uid, array.getJSONObject
+                            (i).getString("deviceId"));
                     if (lightBinding.getBossUid() != null) {
                         pairingService.updateBossBindingInfo(lightBinding
                                 .getBossUid());
@@ -97,7 +106,8 @@ public class AppOperateController extends BaseDecodedController {
     }
 
     @RequestMapping("/open")
-    public void openApp(@ModelAttribute("decodedParams") JSONObject params, ModelMap model) {
+    public void openApp(@ModelAttribute("decodedParams") JSONObject params,
+                        ModelMap model) {
         OpenApp openApp = setOPenApp(params);
         appOperateService.openAppLog(openApp);
         model.put("result", ResultDict.SUCCESS.getCode());
@@ -107,25 +117,52 @@ public class AppOperateController extends BaseDecodedController {
     }
 
     @RequestMapping("/operation")
-    public void LightOperation(@ModelAttribute("decodedParams") JSONObject params, ModelMap model) {
-        LightOperation lightOperation = setLightOperation(params);
-        appOperateService.lightOperationLog(lightOperation);
+    public void LightOperation(@ModelAttribute("decodedParams") JSONObject
+                                           params, ModelMap model) {
+        LightOperation lightOperation = new LightOperation();
+        String uid = params.getString("uid");
+        String mobile = lightUserService.findLightUserByUid(uid);
+        String isRegister;
+        if (StringUtils.isBlank(mobile)) {
+            isRegister = "0";//未注册
+        } else {
+            isRegister = "1";//已注册
+        }
+        JSONArray array = params.getJSONArray("lightGroup");
+        for (int i = 0; i < array.size(); i++) {
+            String deviceId = array.getJSONObject(i).getString("deviceId");
+            String behavior = array.getJSONObject(i).getString("behavior");
+            //开关次数
+            String id = StringUtils.replace(UUID.randomUUID().toString(),
+                    "-", "");
+            lightOperation.setId(id);
+            lightOperation.setUid(uid);
+            lightOperation.setBehavior(behavior);
+            lightOperation.setDevice_id(deviceId);
+            lightOperation.setIsRegister(isRegister);
+            lightOperation.setCreate_date(new Date());
+            long opeDate = array.getJSONObject(i).getLong("opeDate");
+            lightOperation.setOpe_date(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(opeDate)));
+            appOperateService.lightOperationLog(lightOperation);
+        }
         model.put("result", ResultDict.SUCCESS.getCode());
         model.put("result_message", ResultDict.SUCCESS.getValue());
         //生成电费表，beginning...
-        String deviceId = params.getString("deviceId");
-        Map map = new HashMap();
-        try {
-            Map<String, String> binding = (Map) billService.getByDeviceId(deviceId);
-            Map<String, String> operation = (Map) billService.getByLightUid(binding.get("lightUid"));
-            map.put("uid", binding.get("bossUid"));
-            map.put("isRegister", operation.get("is_register"));
-            map.put("result", ResultDict.SUCCESS.getCode());
-            map.putAll(binding);
-        } catch (Exception e) {
-            map.put("result", ResultDict.UID_NOT_EXIST.getCode());
-        }
-        billService.insetBill(map);
+//        String deviceId = params.getString("deviceId");
+//        Map map = new HashMap();
+//        try {
+//            Map<String, String> binding = (Map) billService.getByDeviceId
+//                    (deviceId);
+//            Map<String, String> operation = (Map) billService.getByLightUid
+//                    (binding.get("lightUid"));
+//            map.put("uid", binding.get("bossUid"));
+//            map.put("isRegister", operation.get("is_register"));
+//            map.put("result", ResultDict.SUCCESS.getCode());
+//            map.putAll(binding);
+//        } catch (Exception e) {
+//            map.put("result", ResultDict.UID_NOT_EXIST.getCode());
+//        }
+//        billService.insetBill(map);
         //生成电费表，ending...
     }
 
@@ -139,7 +176,8 @@ public class AppOperateController extends BaseDecodedController {
             behavior = "visit";
         }
         JSONObject firmware = params.getJSONObject("firmware");
-        openApp.setId(StringUtils.replace(UUID.randomUUID().toString(), "-", ""));
+        openApp.setId(StringUtils.replace(UUID.randomUUID().toString(), "-",
+                ""));
         openApp.setUid(uid);
         openApp.setAppid("11");
         openApp.setGroup(params.getString("group"));
@@ -158,26 +196,8 @@ public class AppOperateController extends BaseDecodedController {
         return openApp;
     }
 
-    public LightOperation setLightOperation(JSONObject params) {
-        LightOperation lightOperation = new LightOperation();
-        String uid = params.getString("uid");
-        String deviceId = params.getString("deviceId");
-        String behavior = params.getString("behavior");//'1'为开；'0'为关
-        String id = StringUtils.replace(UUID.randomUUID().toString(), "-", "");
-        String mobile = lightUserService.findLightUserByUid(uid);
-        String isRegister;
-        if (StringUtils.isBlank(mobile)) {
-            isRegister = "0";//未注册
-        } else {
-            isRegister = "1";//已注册
-        }
-        lightOperation.setId(id);
-        lightOperation.setUid(uid);
-        lightOperation.setBehavior(behavior);
-        lightOperation.setDevice_id(deviceId);
-        lightOperation.setIsRegister(isRegister);
-        lightOperation.setCreate_date(new Date());
-        return lightOperation;
-    }
+//    public void setLightOperation(JSONObject params) {
+//
+//    }
 
 }
