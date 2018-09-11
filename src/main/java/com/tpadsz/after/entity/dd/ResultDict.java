cb8426c4d5e9ee06c0dc35e3ccf54aed7488da4d
李楠楠 <nannan.li@tpadsz.cn>
2018-09-11 16:06:23 +0800

    修改电费接口result_message内容

--- src/main/java/com/tpadsz/after/entity/dd/ResultDict.java
+++ src/main/java/com/tpadsz/after/entity/dd/ResultDict.java
@@ -5,7 +5,7 @@ public enum ResultDict {
     SUCCESS("000", "成功"),
     UID_NOT_EXIST("60001", "您的请求参数出错，请稍后重试"),
     DEVICE_UNBIND("60012", "您还未购买灯泡或还未绑定过灯泡或已经解绑"),
-    RECORD_NULL("60013", "您还没有产生电费哦，请根据要求连接灯泡和使用小熊灯控"),
+    RECORD_NULL("60013", "您还没有产生电费哦，请根据要求连接灯泡和使用小熊照明"),
     PARAMS_BLANK("301", "参数不能够为空"), PARAMS_NOT_PARSED("302", "参数解析错误"),
     SYSTEM_ERROR("200", "系统错误"), TIMEOUT("201", "系统超时");
 
