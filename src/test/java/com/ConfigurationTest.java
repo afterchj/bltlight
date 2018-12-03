36ba8c8adb75fe0d34de5e22141e106a312fdc52
hongjian.chen <766256898@qq.com>
2018-12-03 09:28:09 +0800

    。。。

--- src/test/java/com/ConfigurationTest.java
+++ src/test/java/com/ConfigurationTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
