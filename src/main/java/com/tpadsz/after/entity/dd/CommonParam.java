fdcb5d375737fcb015ec756115f54c8ad9676468
hongjian.chen <766256898@qq.com>
2018-12-04 11:34:38 +0800

    修改拦截器匹配p规则

--- src/main/java/com/tpadsz/after/entity/dd/CommonParam.java
+++ src/main/java/com/tpadsz/after/entity/dd/CommonParam.java
@@ -4,8 +4,8 @@ package com.tpadsz.after.entity.dd;
  * Created by hongjian.chen on 2018/11/19.
  */
 public enum CommonParam {
-    APPURL("https://eco.taobao.com/router/rest"), APPKER("25208405"), APPSECRET("d0a228c9cca6bd42a9e0ecd537db0ad1"), //
-    VEKEY("V00000585Y74210916"), VEORDER("http://apiorder.vephp.com/order"), VEHICPI("http://api.vephp.com/hcapi");//
+    APPURL("https://eco.taobao.com/router/rest"), VEORDER("http://apiorder.vephp.com/order"), VEHICPI("http://api.vephp.com/hcapi"), //
+    APPKER("25208405"), APPSECRET("d0a228c9cca6bd42a9e0ecd537db0ad1"), VEKEY("V00000585Y74210916");//
     private String value;
 
     CommonParam(String value) {
