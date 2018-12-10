5ddc27aec943e32d6c062440b5e5304f61ded7e3
hongjian.chen <766256898@qq.com>
2018-12-10 13:22:59 +0800

    修改dubbo端口号

--- src/test/java/MyClient.java
+++ src/test/java/MyClient.java
@@ -27,14 +27,15 @@ public class MyClient {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationConsumer.xml");
         //获取服务器那边的bean
         CoinsEarnerManager earnerManager = (CoinsEarnerManager) ctx.getBean("coinsEarnerManager");
-        CoinsEarnerOffer offer = new CoinsEarnerOffer("9", "9de2725281b44136b04e474d85061151", "电费转积分", 100, UUID.randomUUID().toString().replaceAll("-", ""), CoinsEarnedType.parse(Integer.parseInt("170")));
-        try {
-            earnerManager.earnCoins(offer);
-        } catch (SystemAlgorithmException e) {
-            e.printStackTrace();
-        } catch (CheckNotAllowedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("earnerManager="+earnerManager);
+//        CoinsEarnerOffer offer = new CoinsEarnerOffer("9", "9de2725281b44136b04e474d85061151", "电费转积分", 100, UUID.randomUUID().toString().replaceAll("-", ""), CoinsEarnedType.parse(Integer.parseInt("170")));
+//        try {
+//            earnerManager.earnCoins(offer);
+//        } catch (SystemAlgorithmException e) {
+//            e.printStackTrace();
+//        } catch (CheckNotAllowedException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Test
@@ -42,21 +43,22 @@ public class MyClient {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationConsumer.xml");
         RecordBillService billService = (RecordBillService) ctx.getBean("recordBillService");
         System.out.println("billService=" + billService);
-        Map map = (Map) billService.getSumCharge("0016428081ec495b97edf124cb29d810");
-        System.out.println(JSON.json(map) + "\t" + map.get("total_bill"));
-        Map<String, String> binding = (Map) billService.getByDeviceId("DBD370724B054036B5EF2DAB23128225");
-        System.out.println(binding);
-        Map<String, String> operation = (Map) billService.getByLightUid(binding.get("lightUid"));
-        System.out.println(JSON.json(operation));
 
-        Map map1 = new HashMap();
-        map1.put("uid", binding.get("bossUid"));
-        map1.put("isRegister", operation.get("is_register"));
-        map1.put("status", "000");
-        map1.putAll(binding);
-        System.out.println("map1=" + JSON.json(map1));
+//        Map map = (Map) billService.getSumCharge("0016428081ec495b97edf124cb29d810");
+//        System.out.println(JSON.json(map) + "\t" + map.get("total_bill"));
+//        Map<String, String> binding = (Map) billService.getByDeviceId("DBD370724B054036B5EF2DAB23128225");
+//        System.out.println(binding);
+//        Map<String, String> operation = (Map) billService.getByLightUid(binding.get("lightUid"));
+//        System.out.println(JSON.json(operation));
+//
+//        Map map1 = new HashMap();
+//        map1.put("uid", binding.get("bossUid"));
+//        map1.put("isRegister", operation.get("is_register"));
+//        map1.put("status", "000");
+//        map1.putAll(binding);
+        System.out.println("map1=" + JSON.json(billService.getByDeviceId("dev_121qwe")));
 
-        billService.insetBill(map1);
+//        billService.insetBill(map1);
     }
 
 
